f91d42f9f2ca068d71ef5f479dafc81df765bdb1
Udai Arora <rocketmailudai@gmail.com>
2014-02-20 03:52:33 -0700

    All functionalities implemeted. TODO: Optimizations

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 function post_search(){
-
 	var item=$('.txt').val();
 	console.log("Searched for: " + item);
 
@@ -8,8 +7,37 @@ function post_search(){
 		$(".image-container").html("");
 		data=$.parseJSON(data);
 		$.each(data.results,function() {
-			$(".image-container").append("<img src='"+this.thumbnailImageUrl+"' product='"+this.productId+"' style='"+this.styleId+"'/>");
+			$(".image-container").append("<img class='result' src='"+this.thumbnailImageUrl+"' product='"+this.productId+"' style='"+this.styleId+"'/>");
 		})
-
 	});
+
 }
+
+var watch_list_json={items: []};
+
+function post_watchlist(){
+	$('.discounted-items').html("");
+	console.log("Submitting Watch List to the server");
+
+	$.get('watch', "watch_list_json="+JSON.stringify(watch_list_json) , function(data){
+		data=$.parseJSON(data);
+		var i=0;
+		$.each(data.disc_items,function() {
+			$('.discounted-items').append(data.disc_items[i].percentOff);
+			i++;
+		});
+		
+	});
+	setTimeout(function(){
+		post_watchlist();
+	},5000)
+}
+
+$(document).ready(function(){
+	$('.image-container').on("click", '.result' , function(){
+		var pid=$(this).attr("product");
+		var sid=$(this).attr("style");
+		watch_list_json.items.push({"productId":pid, "styleId":sid});
+		$('.watch-list').append(this);
+	})
+});
--- server.js
+++ server.js
@@ -8,17 +8,19 @@ app.use('/styles', express.static(__dirname + '/styles'));
 app.use('/js', express.static(__dirname + '/js'));
 
 app.get('/', function(req, res) {
-    res.sendfile('./views/index.html');
+	res.sendfile('./views/index.html');
 });
 
 app.get('/fetch', function(req, res) {
 	
-	var url="/Search?limit=5&key=52ddafbe3ee659bad97fcce7c53592916a6bfd73&term="+url_par.format(req.query.item),
+	var url="/Search?limit=20&key=52ddafbe3ee659bad97fcce7c53592916a6bfd73&term="+url_par.format(req.query.item);
 	//var url="/Product/7925931?key=52ddafbe3ee659bad97fcce7c53592916a6bfd73",
 
 		//create the options to pass into the get request
 		options={
-			host:"api.zappos.com" ,path:url};
+			host:"api.zappos.com",
+			path:url
+		};
 			//host:"localhost:24789" ,path:url};
 
 	//a little lightweight logging to watch requests
@@ -28,22 +30,96 @@ app.get('/fetch', function(req, res) {
 	//make the request server side
 	http.get(options,function(response){
 		res.writeHead(200, {'Content-Type': 'text/plain'});
-		var pageData = "";
+		var responseData = "";
 		response.setEncoding('utf8');
 	    //stream the data into the response
 	    response.on('data', function (chunk) {
-	    	pageData += chunk;
+	    	responseData += chunk;
 	    });
 
 	    //write the data at the end
 	    response.on('end', function(){
-	    	res.write(pageData);
+	    	res.write(responseData);
 	    	res.end();
 	    });
 
 	});
 
 });
+
+
+app.get('/watch', function(req, res) {
+
+	//a little lightweight logging to watch requests
+	//console.log(req.query.watch_list_json);
+	//console.log("requrl:",req.url);
+
+	var data=JSON.parse(req.query.watch_list_json);
+
+	var url;
+	var discounted_json={disc_items: []};
+	var req_count=0;
+
+	for(var num in data.items)
+	{
+
+		url ='/Product/styleId/'+data.items[num].styleId+'?&key=52ddafbe3ee659bad97fcce7c53592916a6bfd73&includes=["styles"]';
+		options={
+			host:"api.zappos.com",
+			path:url
+		};
+
+		try{
+			//make the request server side to check for the products percentageOff
+			http.get(options,function(response){
+				req_count++;
+				var responseData = "";
+				response.setEncoding('utf8');
+			    //stream the data into the response
+			    response.on('data', function (chunk) {
+			    	responseData+=chunk.toString();
+			    });
+			    //responseData=JSON.parse(responseData);
+			    //write the data at the end
+			    response.on('end', function(){
+			    	
+			    	body=JSON.parse(responseData);
+			    	try{
+			    		var discount=body.product[0].styles[0].percentOff.replace('%','');
+			    	} catch(err) {
+			    		console.log(err);
+			    	}
+			    	
+			    	
+			    	if(discount>=20)
+			    	{
+			    		discounted_json.disc_items.push({"percentOff":discount});
+			    	}
+
+			    	if(data.items.length==req_count)
+			    	{
+			    		wr();
+			    	}
+
+			    });
+
+			});
+		} catch(e) {
+			res.writeHead(200, {'Content-Type': 'text/plain'});
+			res.write("Something went wrong while trying to fetch the discounts.");
+			console.log("Something went wrong while trying to fetch the discounts.");
+			res.end();
+		}
+	}
+	function wr(){
+		res.writeHead(200, {'Content-Type': 'text/plain'});
+		res.write(JSON.stringify(discounted_json));
+		res.end();
+	}
+
+});
+
+
 
 
 app.listen(24789);
